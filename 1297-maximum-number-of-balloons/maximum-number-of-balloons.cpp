050ccad38438cde56ef837818c006a0ee4832a69
Divya upadhyay <99160883+divyaupadhyay56@users.noreply.github.com>
2025-06-30 21:41:10 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (33.99%) - LeetSync

---
+++ 1297-maximum-number-of-balloons/maximum-number-of-balloons.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int maxNumberOfBalloons(string text) {
+        unordered_map<char,int> mp;
+        for(int i=0;i<text.size();i++){
+            mp[text[i]]++;
+        }
+        return min({mp['b'],mp['a'],mp['l']/2,mp['l']/2,mp['o']/2,mp['o']/2,mp['n']});
+    }
+};
