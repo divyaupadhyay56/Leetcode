9571a28d19d83b5141344d3c01ab08b599355eee
Divya upadhyay <99160883+divyaupadhyay56@users.noreply.github.com>
2025-12-19 21:41:17 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (98.13%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int dp[46];   
+
+    int solve(int n) {
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+
+        if (dp[n] != -1) return dp[n];
+
+        return dp[n] = solve(n - 1) + solve(n - 2);
+    }
+
+    int climbStairs(int n) {
+        memset(dp, -1, sizeof(dp)); //memset ek C/C++ standard function hai jo 
+        //memory ke har byte ko same value se fill kar deta hai.
+        return solve(n);
+    }
+};
