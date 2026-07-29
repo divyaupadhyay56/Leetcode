76042068b8a2a7d5ad8bf3aa1329a3a3007f707d
Divya upadhyay <99160883+divyaupadhyay56@users.noreply.github.com>
2026-07-29 17:22:34 +0530

    Time: 67 ms (86.87%) | Memory: 13.5 MB (81.26%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,19 +1,27 @@
 class Solution {
 public:
-    bool canPartition(vector<int>& nums) {
-        int sum = 0;
-        for(int x : nums) sum += x;
-        if(sum % 2 != 0) return false;
-        int target = sum / 2;
-        vector<bool> dp(target + 1, false);
-        dp[0] = true; // base case
-
-        for(int num : nums) {
-            for(int s = target; s >= num; s--) {
-                dp[s] = dp[s] || dp[s - num];
+    int subset_sum(vector<int>& nums, int half_sum, int n){
+        vector<bool> dp(half_sum + 1, false);
+        dp[0] = true;
+        for (int i = 0; i < n; i++) {
+            for (int j = half_sum; j >= nums[i]; j--) {
+                if (dp[j - nums[i]]) {
+                    dp[j] = true;
+                }
             }
         }
+        return dp[half_sum];
+    }
+    bool canPartition(vector<int>& nums) {
+        int n = nums.size();
+        int total_sum = 0;
+        for(int i=0;i<nums.size();i++){
+            total_sum += nums[i];
+        }
+        if(total_sum%2 != 0)return false;
+        int half_sum = total_sum/2;
 
-        return dp[target];
+        return subset_sum(nums,half_sum,n);
+
     }
 };
